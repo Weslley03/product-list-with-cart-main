e8b5b1aa57c74105e529a954aa6580bc0fe52ba2
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-18 14:47:37 -0300

    project

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <section class="container">
+
+    <div class="section-grid">
+      
+      <div class="title">
+        <h1> Desserts </h1>
+      </div>
+  
+      <ProductCard />
+  
+    </div>
+  
+  </section>
+
+</template>
+
+<script>
+import ProductCard from './components/ProductCard.vue';
+
+export default {
+  components: {
+    ProductCard,
+  }
+}  
+</script>
+
+<style>
+
+  .container {
+    font-family: 'Red Hat Text';
+    margin-top: 1%;    
+    margin-left: 8%;
+    margin-right: 8%;
+    margin-bottom: 2%;
+  }
+
+  .section-grid {
+    margin: 5%;    
+    border-radius: 10px;
+    padding: 3rem;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    display: grid;
+    grid-template-columns: repeat(3, auto) ;
+    grid-template-rows: repeat(4, auto);
+    gap:  1rem;
+  }
+
+  .section-grid>* {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .title {
+    grid-column: 1 / -1;
+    justify-content: start;
+  }
+
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <section
+    v-for="(product, index) in products"
+    :key="index"
+  >
+    <div class="cardSection">
+      <img 
+      :src="product.image.desktop"
+      class="imgProduct"
+      /> 
+      <button class="buttonAddCard"> add to Card </button>
+    </div>
+
+    <span class="category"> {{ product.category }} </span>
+    <h2> {{ product.name }} </h2>
+    <span class="price"> R$ {{ product.price }} </span>
+  
+  </section>
+</template>
+
+<script>
+  export default {
+   
+    data() {
+      return {
+        products: [],
+      }
+    },
+
+    async mounted() {
+      await this.fetchData();
+    },
+
+    methods: {
+      async fetchData() {
+        const response = await fetch('../../data.json');
+        this.products = await response.json();
+        console.log(this.products)
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+  section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .cardSection {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    display: flex;
+    width: 100%;
+  }
+
+  .buttonAddCard {
+    position: absolute;
+    border-radius: 20px;
+    width: 10%;
+    margin-top: 21%;
+    padding: 0.5rem
+  }
+
+  .category {
+    margin-top: 3rem;
+    font-size: 100%;
+  }
+
+  h2 {
+    font-size: 100%;
+  }
+
+  .price {
+    font-size: 100%;
+  }
+</style>
